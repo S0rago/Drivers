bf9de020f84e60a1f2069cce153a9c6285693e39
S0rago <linehalt10909@yandex.ru>
2019-11-09 09:44:53 +0300

    v0.2 Добавлены корректные пути

--- sync.sh
+++ sync.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Starting sync...
-src="~/Downloads/ToSync/"
-dest="/media/soragomint/SP\ UFD\ U3/"
+echo "Starting sync..."
+src="/home/soragomint/Downloads/ToSync/"
+dest="/media/soragomint/SP UFD U3"
 	
-if mount | grep -q ' on  $dest'; then
+if [ -n $(mount | grep -q ' on  $dest') ]; then
  echo "Drive found"
- echo "Copying..."
- cp -ru "$src*" "$dest/ToSync/"
- cp -ru "$dest/ToSync/*" "$src"
+ echo "From $src"
+ echo "to $dest"
+ cp -ru "$src"* "$dest/ToSync/"
+ cp -ru "$dest/ToSync/"* "$src"
 fi
 
 #else mount usb on /media/*MOUNT_POINT*/
 echo "DONE"
+
+exit 0
